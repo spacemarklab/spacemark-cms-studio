c63dd508b56cd85b079039b988d041ef5f05bb9a
Tez <tez@Tezs-MacBook-Pro.local>
2025-02-13 22:33:32 +0000

    Added schema to Sanity

--- schemaTypes/index.js
+++ schemaTypes/index.js
@@ -1,1 +1,15 @@
-export const schemaTypes = []
+import { defineType } from 'sanity'
+
+export const schemaTypes = [
+  defineType({
+    name: 'post',
+    title: 'Post',
+    type: 'document',
+    fields: [
+      { name: 'title', title: 'Title', type: 'string' },
+      { name: 'slug', title: 'Slug', type: 'slug', options: { source: 'title', maxLength: 96 } },
+      { name: 'body', title: 'Body', type: 'text' },
+    ],
+  }),
+];
+
